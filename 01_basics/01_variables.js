32ab4802d388611d678726dc993b4bc217eb31c5
MaheshYogi007 <yogimahesh8692@gmail.com>
2024-07-04 17:29:12 +0530

    iital setup is complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 1234
+let accountEmail = "mahesh@google.com"
+var accountPassword = "1235"
+var accountCity = "ahemadabad",
+accoutState = "active";
+let ifc;
+var pincod;
+// accountId = 2
+
+accountEmail = "ma@ma.com"
+accountPassword = "5556"
+accountCity = "bhilwara"
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accoutState,ifc,pincod,aci])
