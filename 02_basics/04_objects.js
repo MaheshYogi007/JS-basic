0e337135f48ab532d4af85a2d45f4e14ef382a41
MaheshYogi007 <yogimahesh8692@gmail.com>
2024-08-01 16:07:28 +0530

    Objects and jason talks

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -16,9 +16,53 @@ const regularUser = {
     fullname: {
         userfullname: {
             firstname: "Mahesh",
-            lastname: "Yogi"
+            lastname: "Yogi",
         }
     }
 }
 
-console.log(regularUser.fullname,);
+console.log(regularUser.fullname.userfullname);
+
+
+const obj1 = {1: "a",2: "b"}
+const obj2 = {3: "a",4: "b"}
+const obj4 = {5: "a",6: "b"}
+
+
+//const obj3 = Object.assign({},obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);  
+
+
+const users = [
+    {
+       // id : 1
+       // email : "xyz@gmail.com"
+    },
+    {
+    },
+    {
+    },
+
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('name'));
+
+
+const course = {
+    coursename : "Js in Hindi",
+    price : "999",
+    courseInstuctor: "Hitesh"
+}
+
+//const{courseInstuctor} = course
+
+const{courseInstuctor: instuctor} = course
+console.log(instuctor);
